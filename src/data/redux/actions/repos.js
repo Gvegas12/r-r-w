b5b9c5a4369a801486817d56b0bc2ac21117da27
abdukarimov.h2 <abdukarimov.h2@wildberries.work>
2023-09-22 02:27:04 +0300

    feat: add pagination

--- src/data/redux/actions/repos.js
+++ src/data/redux/actions/repos.js
@@ -3,7 +3,11 @@ import { setIsFetching, setRepos } from "../reducers/reposReducer";
 
 const searchQueryDefaultValue = "stars:%3E1";
 
-export const getRepos = (searchQuery = searchQueryDefaultValue) => {
+export const getRepos = (
+  searchQuery = searchQueryDefaultValue,
+  currentPage = 1,
+  perPage = 10
+) => {
   if (!searchQuery) {
     searchQuery = searchQueryDefaultValue;
   }
@@ -11,7 +15,7 @@ export const getRepos = (searchQuery = searchQueryDefaultValue) => {
   return async (dispatch) => {
     dispatch(setIsFetching(true));
     const { data } = await axios.get(
-      `https://api.github.com/search/repositories?q=${searchQuery}&sort=stars`
+      `https://api.github.com/search/repositories?q=${searchQuery}&sort=stars&per_page=${perPage}&page=${currentPage}`
     );
     dispatch(setRepos(data));
   };
--- src/view/components/MainPage/MainPage.jsx
+++ src/view/components/MainPage/MainPage.jsx
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { Repo } from "./Repo/Repo";
 import { getRepos } from "../../../data/redux/actions/repos";
 
 import "./main.less";
+import { setCurrentPage } from "../../../data/redux/reducers/reposReducer";
+import { createPages } from "../../../data/utils/createPages";
 
 export const MainPage = () => {
   const dispatch = useDispatch();
-  const { items: repos, isFetching } = useSelector((state) => state.repos);
-
+  const {
+    items: repos,
+    isFetching,
+    currentPage,
+    totalCount,
+    perPage,
+  } = useSelector((state) => state.repos);
   const [search, setSearch] = React.useState("");
 
+  const pagesCount = Math.ceil(totalCount / perPage);
+  const pages = [];
+  createPages(pages, pagesCount, currentPage);
+
   React.useEffect(() => {
-    dispatch(getRepos());
-  }, []);
+    dispatch(getRepos(search, currentPage, perPage));
+  }, [currentPage]);
 
   const onChangeSearch = (e) => {
     setSearch(e.target.value);
   };
 
   const onSearchHandler = () => {
+    dispatch(setCurrentPage(1));
     dispatch(getRepos(search));
-  }
+  };
+
+  const onClickPage = (e) => {
+    dispatch(setCurrentPage(Number(e.target.innerHTML)));
+  };
 
   return (
     <div className="main">
@@ -33,7 +49,9 @@ export const MainPage = () => {
           type="text"
           className="search-input"
         />
-        <button onClick={onSearchHandler} className="search-btn">Search</button>
+        <button onClick={onSearchHandler} className="search-btn">
+          Search
+        </button>
       </div>
 
       {isFetching ? (
@@ -41,6 +59,18 @@ export const MainPage = () => {
       ) : (
         repos.map((item) => <Repo key={item.id} repo={item} />)
       )}
+
+      <div className="pages">
+        {pages.map((page, i) => (
+          <span
+            key={i}
+            className={currentPage === page ? "current-page" : "page"}
+            onClick={onClickPage}
+          >
+            {page}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
